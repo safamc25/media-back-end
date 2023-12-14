28f49848e4c4a8acf1ba65b3a18c8b9dd5583b20
safamc25 <safamc25>
2023-12-14 10:10:09 +0530

    mediaplayer

---
+++ index.js
@@ -0,0 +1,34 @@
+// import json server using require method
+
+const jsonServer=require('json-server')
+
+// to create server using json server
+
+const server=jsonServer.create()
+
+
+// route json data
+
+const router=jsonServer.router("db.json")
+
+// create middle ware
+
+const middleware=jsonServer.defaults()
+
+// create port for server
+
+const PORT=process.env.PORT || 4000
+
+// use  middle ware in  server
+
+server.use(middleware)
+server.use(router)
+
+
+// to use port in server 
+
+server.listen(PORT,()=>{
+    console.log("media player server started at PORT"+PORT);
+})
+
+
